a3a1f287f4314049570111bd307f0953fab69939
João Carlos Silva <Joo Carlos Silva>
2016-09-25 19:14:56 +0100

    Initial commit

---
+++ app/src/main/java/pt/ipca/createnewactivity/MainActivity.java
@@ -0,0 +1,24 @@
+package pt.ipca.createnewactivity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "pt.ipca.createNewActivity.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
